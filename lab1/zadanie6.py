0707be40158896a6a7d5553db009ddbe7d2c2798
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-03-07 19:36:41 +0100

    lab1 - zadanie6

---
+++ lab1/zadanie6.py
@@ -0,0 +1,8 @@
+string = """I've been here all night
+    I've been here all day
+    And boy, got me walkin' side to side
+    I've been here all night
+    I've been here all day
+    And boy, got me walkin' side to side (Side to side)"""
+
+print(string.count('been'))
