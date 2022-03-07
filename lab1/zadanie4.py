acb1926deae7429c2e9318ca150f67e0068e7cc0
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-03-07 19:25:28 +0100

    lab1 - zadanie4

---
+++ lab1/zadanie4.py
@@ -0,0 +1,14 @@
+import math
+
+e_to_ten = math.e ** 10
+element_of_logarithm = math.log(5 + math.sin(8) ** 2) ** (1 / 6)
+floor = math.floor(3.55)
+ceil = math.ceil(4.80)
+
+print(e_to_ten)
+print(element_of_logarithm)
+print(floor)
+print(ceil) 
+
+
+
