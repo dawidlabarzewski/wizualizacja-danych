0a88a12577d14591eaac8a695cfe08140d76c363
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-03-07 19:13:50 +0100

    lab1 - zadanie2

---
+++ lab1/zadanie2.py
@@ -0,0 +1,21 @@
+a = 1
+b = 2
+c = 3
+
+addition = a + b
+subtraction = c - a
+division = b / a
+multiplication = c * b
+integer_division = c // b
+rest_of_division = c % b
+exponentiation = b ** c
+exponentiation2 = pow(b, c)
+
+print(addition)
+print(subtraction)
+print(division)
+print(multiplication)
+print(integer_division)
+print(rest_of_division)
+print(exponentiation)
+print(exponentiation2)
