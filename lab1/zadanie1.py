78efa2d499f502ec2572aeb8ac2af13d416b63de
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-03-07 19:08:15 +0100

    lab1 - zadanie1

---
+++ lab1/zadanie1.py
@@ -0,0 +1,20 @@
+string1 = 'asd'
+string2 = 'def'
+
+int1 = 12
+int2 = 2137
+
+float1 = 1.0
+float2 = 12.32
+
+complex1 = 4+2j
+complex2 = 1+1j
+
+print(f'string: {string1}')
+print(f'string: {string2}')
+print(f'int: {int1}')
+print(f'int: {int2}')
+print(f'float: {float1}')
+print(f'float: {float2}')
+print(f'complex: {complex1}')
+print(f'complex: {complex2}')
