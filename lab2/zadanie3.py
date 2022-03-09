98aa32694e5637debd473f9200d8492429160a0e
dawidlabarzewski <154749@student.uwm.edu.pl>
2022-03-09 18:40:00 +0100

    lab2 - zadanie3

---
+++ lab2/zadanie3.py
@@ -0,0 +1,9 @@
+games = {
+    'FC3': 'Far Cry 3',
+    'NFSMW': 'Need For Speed Most Wanted',
+    'CMR5': 'Colin McRae Rally 2005',
+    'CSGO': 'Counter Strike Global Offensive',
+    'TM': 'Trackmania'
+}
+
+print(len(games.keys()))
